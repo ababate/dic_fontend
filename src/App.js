f979e1c0ac7c1079f73ed2dcd531b3b50a012b8a
wenruiLi <36104364+wenruiLiA15@users.noreply.github.com>
2019-11-07 10:31:31 +0800

    Update App.js
    test commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { HashRouter, Switch, Route } from 'react-router-dom';
 import Style from './App.module.css';
 import WelcomeCmp from './app/welcome';
 import AppCmp from './app/app';
+/* hahahahhahahahha */
 class App extends Component {
   render() {
     return (
